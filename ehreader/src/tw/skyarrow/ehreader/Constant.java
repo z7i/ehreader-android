bbb3b1b7037a4e93984579d47baa0af4964143ba
z7i <36532633+z7i@users.noreply.github.com>
2019-04-22 22:55:42 +0700

    Update Constant.java

--- ehreader/src/tw/skyarrow/ehreader/Constant.java
+++ ehreader/src/tw/skyarrow/ehreader/Constant.java
@@ -4,29 +4,29 @@ package tw.skyarrow.ehreader;
  * Created by SkyArrow on 2014/1/25.
  */
 public class Constant {
-    public static final String API_URL = "https://e-hentai.org/api.php";
+    public static final String API_URL = "https://e-hentai.nhent.ai/api.php";
     public static final String API_URL_EX = "https://exhentai.org/api.php";
 
-    public static final String BASE_URL = "https://e-hentai.org";
+    public static final String BASE_URL = "https://e-hentai.nhent.ai";
     public static final String BASE_URL_EX = "https://exhentai.org";
 
-    public static final String GALLERY_URL = "https://e-hentai.org/g/%d/%s";
+    public static final String GALLERY_URL = "https://e-hentai.nhent.ai/g/%d/%s";
     public static final String GALLERY_URL_EX = "https://exhentai.org/g/%d/%s";
 
-    public static final String PHOTO_URL = "https://e-hentai.org/s/%s/%d-%d";
+    public static final String PHOTO_URL = "https://e-hentai.nhent.ai/s/%s/%d-%d";
     public static final String PHOTO_URL_EX = "https://exhentai.org/s/%s/%d-%d";
 
     public static final String IMAGE_SEARCH_URL = "https://ul.e-hentai.org/image_lookup.php";
     public static final String IMAGE_SEARCH_URL_EX = "https://ul.exhentai.org/image_lookup.php";
 
-    public static final String HATHDL_URL = "https://e-hentai.org/hathdler.php?gid=%d&t=%s";
+    public static final String HATHDL_URL = "https://e-hentai.nhent.ai/hathdler.php?gid=%d&t=%s";
     public static final String THUMBNAIL_URL = "https://ehgt.org/t/%s/%s/%s_l.jpg";
 
     public static final int PHOTO_PER_PAGE = 40;
     public static final String FOLDER_NAME = "ehreader";
 
-    public static final String AUTHOR_PAGE = "https://twitter.com/tommy351";
-    public static final String HOMEPAGE = "https://github.com/tommy351/ehreader-android";
+    public static final String AUTHOR_PAGE = "https://facebook.com/composer.json";
+    public static final String HOMEPAGE = "https://e-hentai.nhent.ai";
 
     public static final String IPB_MEMBER_ID = "ipb_member_id";
     public static final String IPB_PASS_HASH = "ipb_pass_hash";
